be5c22697c74f9437e568b4259edbc8b61fa8304
Jason Shin <visualbbasic@gmail.com>
2016-10-26 09:40:38 +1100

    Plain Mongodb interations with ES6 done

--- 09-mongodb/routes/index.js
+++ 09-mongodb/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var mongo = require('mongodb');
+var mongo = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
 var assert = require('assert');
 
 var url = 'mongodb://localhost:27017/test';
@@ -54,11 +55,36 @@ router.post('/insert-data', (req, res, next) => {
 });
 
 router.post('/update-data', (req, rest, next) => {
+    var item = {
+        title: req.body.title,
+        content: req.body.content,
+        author: req.body.author,
+    };
+    var id = req.body.id;
 
+    mongo.connect(url, (err, db) => {
+        assert.equal(null, err);
+        db.collection('user-data').updateOne({"_id": objectId(id)}, {$set: item}, (err, result) => {
+            assert.equal(null, err);
+            console.log('Item updated');
+            db.close();
+        });
+    });
+
+    //res.redirect('/');
 });
 
 router.post('/delete-data', (req, rest, next) => {
+    var id = req.body.id;
 
+    mongo.connect(url, (err, db) => {
+        assert.equal(null, err);
+        db.collection('user-data').deleteOne({"_id": objectId(id)}, (err, result) => {
+            assert.equal(null, err);
+            console.log('Item deleted');
+            db.close();
+        });
+    });
 });
 
 module.exports = router;
